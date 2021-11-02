40d493040cb285a57312e00b23e53f98012e01a9
5at05h1 <69383363+5at05h1@users.noreply.github.com>
2021-11-02 15:23:01 +0900

    編集画面追加

--- FormApp/urls.py
+++ FormApp/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'form_app'
 
 urlpatterns = [
-    path('insert_student', views.insert_student, name='insert_student')
+    path('insert_student', views.insert_student, name='insert_student'),
+    path('students_list', views.students_list, name='students_list'),
+    path('update_student/<int:id>', views.update_student, name='update_student')
 ]
--- FormApp/views.py
+++ FormApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import forms
+from .models import Students
 # Create your views here.
 
 
@@ -11,5 +12,29 @@ def insert_student(request):
     return render(
         request, 'form_app/insert_student.html', context={
             'insert_form': insert_form
+        }
+    )
+
+def students_list(request):
+    students = Students.objects.all()
+    return render(
+        request, 'form_app/students_list.html', context={
+            'students': students
+        }
+    )
+
+def update_student(request, id):
+    student = Students.objects.get(id=id)
+    update_form = forms.StudentUpdateForm(
+        initial = {
+            'name': student.name, 'age': student.age, 'grade': student.grade, 'picture': student.picture
+        }
+    )
+    if request.method == 'POST':
+        pass
+    return render(
+        request, 'form_app/update_student.html', context={
+            'update_form': update_form,
+            'student': student
         }
     )
