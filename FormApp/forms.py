ecba4cb466248f87a5ccdc5f1f115c9172306e1f
5at05h1 <69383363+5at05h1@users.noreply.github.com>
2021-11-02 16:02:21 +0900

    削除画面

--- FormApp/forms.py
+++ FormApp/forms.py
@@ -19,3 +19,6 @@ class StudentUpdateForm(forms.Form):
     age = forms.IntegerField(label='年齢')
     grade = forms.IntegerField(label='学年')
     picture = forms.FileField(label='ファイルアップロード')
+
+class StudentDeleteForm(forms.Form):
+    id = forms.IntegerField(widget=forms.HiddenInput)
--- FormApp/urls.py
+++ FormApp/urls.py
@@ -6,5 +6,6 @@ app_name = 'form_app'
 urlpatterns = [
     path('insert_student', views.insert_student, name='insert_student'),
     path('students_list', views.students_list, name='students_list'),
-    path('update_student/<int:id>', views.update_student, name='update_student')
+    path('update_student/<int:id>', views.update_student, name='update_student'),
+    path('delete_student/<int:id>', views.delete_student, name='delete_student')
 ]
